165fd2e9701f3cd81ebc4ed1a7145c536a533d39
Rubya Rodrigues <141880164+rubya87@users.noreply.github.com>
2024-09-05 22:54:05 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flasck(___name__)
+
+@pp.route('/inicio') 
+
+def ola():
+return render_template('index.html'); #coloco o nome do arquivo de html entre aspas
+
+app.run()
